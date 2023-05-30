ec539df0fed288149060c4320e5b814e62e3cde8
Challucas <lucas.chalscl@gmail.com>
2023-05-30 11:59:56 +0200

    faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,24 @@
 
 namespace App\DataFixtures;
 use App\Entity\Todo;
+use App\Repository\TodoRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        
-         // create 20 todos! Bam!
-         for ($i = 0; $i < 50; $i++) {
-            $todo = new Todo();
-            $todo->setName('todo '.$i);
-            $todo->setDescription(mt_rand(10, 100));
-            $manager->persist($todo);
+        $faker = Faker\Factory::create('fr_FR');
+        $todo = Array();
+        for ($i = 0; $i < 50; $i++) {
+            $todo[$i] = new Todo();
+            $todo[$i]->setName($faker->name);
+            $todo[$i]->setDescription('description '.$i);
+
+            $manager->persist($todo[$i]);
+       
         }
 
         $manager->flush();
--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -17,7 +17,7 @@ class Todo
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(type: Types::TEXT)]
+    #[ORM\Column(type: Types::TEXT, length: 999)]
     private ?string $description = null;
 
     public function getId(): ?int
